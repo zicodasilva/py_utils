6db4ac45981cfe2a1d6d78ce0073db23b6c64a9d
Zico <zicods7@gmail.com>
2021-04-26 13:22:36 +0100

    Created package and installed via "pip install -e ."

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py_utils",
+    version="1.0.0",
+    description="A general shared utilities library for Python.",
+    author="Zico da Silva",
+    author_email="zicods7@gmail.com",
+    url="https://github.com/zicodasilva/py_utils",
+    packages=find_packages(),
+    install_requires=["cloudpickle"]
+)

b7c1f7a6e633a30abcf42d813650ddc852ac6f95
Zico <zicods7@gmail.com>
2021-08-07 12:02:57 +0100

    Code clean up.

--- py_utils/data_ops.py
+++ py_utils/data_ops.py
@@ -11,6 +11,7 @@ from typing import Any, Dict, List, Union
 import pandas as pd
 import numpy as np
 
+
 def save_pickle(filename: str, data: Dict) -> None:
     """Saves dictionary as a pickle file to a user supplied destination directory.
 
@@ -20,6 +21,7 @@ def save_pickle(filename: str, data: Dict) -> None:
     """
     with open(filename, "wb") as handle:
         pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 
 def load_pickle(filename: str) -> Dict:
     """Reads data from a pickle file.
@@ -33,6 +35,7 @@ def load_pickle(filename: str) -> Dict:
     with open(filename, "rb") as handle:
         return pickle.load(handle)
 
+
 def save_dill(filename: str, data: Any) -> None:
     """Saves dictionary as a pickle file to a user supplied destination directory using dill.
 
@@ -42,6 +45,7 @@ def save_dill(filename: str, data: Any) -> None:
     """
     with open(filename, "wb") as handle:
         dill.dump(data, handle, protocol=pickle.DEFAULT_PROTOCOL)
+
 
 def load_dill(filename: str) -> Dict:
     """Reads data from a pickle file using dill.
@@ -55,6 +59,7 @@ def load_dill(filename: str) -> Dict:
     with open(filename, "rb") as handle:
         return dill.load(handle)
 
+
 def save_cloudpickle(filename: str, data: Any) -> None:
     """Saves the data that cannot be saved with the normal `pickle` module i.e. sympy functions and pyomo models.
 
@@ -64,6 +69,7 @@ def save_cloudpickle(filename: str, data: Any) -> None:
     """
     with open(filename, "wb") as file:
         cloudpickle.dump(data, file, protocol=pickle.DEFAULT_PROTOCOL)
+
 
 def load_cloudpickle(filename: str) -> Dict:
     """Reads data using cloudpickle.
@@ -76,6 +82,7 @@ def load_cloudpickle(filename: str) -> Dict:
     """
     with open(filename, "rb") as handle:
         return cloudpickle.load(handle)
+
 
 def get_key(dict_data: Dict, value: Any) -> str:
     """Gets the key value in a dictionary based on the value.
@@ -90,13 +97,14 @@ def get_key(dict_data: Dict, value: Any) -> str:
     Returns: the key value.
     """
     for key, val in dict_data.items():
-         if val == value:
-             return key
+        if val == value:
+            return key
 
     raise ValueError(f"Could not find key corresponding to value: {value}")
 
-def series_to_supervised(data: Union[pd.DataFrame, np.ndarray, List], n_in = 1, n_out = 1, dropnan = True) -> pd.DataFrame:
-	"""
+
+def series_to_supervised(data: Union[pd.DataFrame, np.ndarray, List], n_in=1, n_out=1, dropnan=True) -> pd.DataFrame:
+    """
 	Frame a time series as a supervised learning dataset.
 	Args:
 		data: Sequence of observations as a list or NumPy array.
@@ -106,25 +114,25 @@ def series_to_supervised(data: Union[pd.DataFrame, np.ndarray, List], n_in = 1, 
 	Returns:
         Pandas DataFrame of series framed for supervised learning.
 	"""
-	n_vars = 1 if type(data) is list else data.shape[1]
-	df = pd.DataFrame(data)
-	cols, names = list(), list()
-	# input sequence (t-n, ... t-1)
-	for i in range(n_in, 0, -1):
-		cols.append(df.shift(i))
-		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-	# forecast sequence (t, t+1, ... t+n)
-	for i in range(0, n_out):
-		cols.append(df.shift(-i))
-		if i == 0:
-			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-		else:
-			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-	# put it all together
-	agg = pd.concat(cols, axis=1)
-	agg.columns = names
-	# drop rows with NaN values
-	if dropnan:
-		agg.dropna(inplace=True)
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
 
-	return agg
+    return agg
